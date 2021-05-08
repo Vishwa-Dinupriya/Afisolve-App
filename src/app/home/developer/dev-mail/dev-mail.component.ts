8705af38844339d113623683e131e4b174eca99c
A-Chathumini <ackatuwawala@gmail.com>
2021-05-08 19:37:16 +0530

    Modified Developer

---
+++ src/app/home/developer/dev-mail/dev-mail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dev-mail',
+  templateUrl: './dev-mail.component.html',
+  styleUrls: ['./dev-mail.component.css']
+})
+export class DevMailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
